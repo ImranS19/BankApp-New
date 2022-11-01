b0f17f677c90b973c24088b703b634d3064838e0
Imran <imran.sadik@outlook.com>
2022-11-01 19:21:15 -0400

    Add project files.

---
+++ BankApp/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+
+    enum TypeOfAccounts
+    {
+        Checking, 
+        Savings,
+        CD,
+        Loan
+    } 
+    /// <summary>
+    /// This represents a bank account where you can withdraw or deposit money into the account
+    /// </summary>
+    internal class Account
+    {
+        private static int lastAccountNumber = 0;
+        #region Properties
+        /// <summary>
+        /// Unique account number
+        /// </summary>
+        public int AccountNumber { get; private set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; private set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreatedDate { get; set; }
+        #endregion
+
+        #region Constructor 
+        public Account()
+        {
+            // lastAccountNumber += 1;
+            AccountNumber = ++lastAccountNumber;
+            CreatedDate = DateTime.UtcNow;
+        }
+        #endregion
+        #region Methods
+        /// <summary>
+        /// Deposit money into the account
+        /// </summary>
+        /// <param name="amount">Amount to deposit</param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+        #endregion
+
+
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BankApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new Account();
+            // myAccount.AccountNumber = 123456;
+            myAccount.AccountName = "My checking";
+            // myAccount.Balance = 10000000000000000;
+            myAccount.Deposit(1001);
+            Console.WriteLine($"AN: {myAccount.AccountNumber}, Balance: {myAccount.Balance}, CD: {myAccount.CreatedDate}");
+
+            var myAccount2 = new Account();
+            Console.WriteLine($"AN: {myAccount2.AccountNumber}, Balance: {myAccount2.Balance}, CD: {myAccount2.CreatedDate}");
+
+
+        }
+    }
+}
